180401c659f19a0e7b6bed6026524a6c6da5ba18
aridosmarraque-droid <aridosmarraque@gmail.com>
2026-08-10 22:27:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Header, AppMode } from './components/Header.tsx';
-import { OperatorMobileView } from './components/OperatorMobileView.tsx';
-import { ClientPortalView } from './components/ClientPortalView.tsx';
-import { AdminPlantView } from './components/AdminPlantView.tsx';
-import { SettingsModal } from './components/SettingsModal.tsx';
+import { Header, AppMode } from './components/Header';
+import { OperatorMobileView } from './components/OperatorMobileView';
+import { ClientPortalView } from './components/ClientPortalView';
+import { AdminPlantView } from './components/AdminPlantView';
+import { SettingsModal } from './components/SettingsModal';
 import { RCDService } from './services/rcdStorage';
 import { SupabaseService } from './services/supabaseClient';
 import { Client, Albaran, Certificate } from './types/rcd';
